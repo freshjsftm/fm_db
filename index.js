ba76017abdd15532114f25931a759f3cfc41c95a
Elena <freshjs.ftm@gmail.com>
2022-01-04 12:07:27 +0200

    Insert user

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-fetch
-
-
-
-
 const { Client } = require('pg');
 
 const config = {
@@ -13,12 +8,25 @@ const config = {
   database: 'fm_test'
 };
 
+const user = {
+  first_name: 'Tom',
+  last_name: 'Bom',
+  email: 'Tom@gmail.com',
+  is_male: true,
+  birthday: '1961-06-28',
+  height: 1.85,
+}
+
 const client = new Client(config);
 start();
 
 async function start(){
   await client.connect();
-  const res = await client.query();
+  const res = await client.query(`
+    INSERT INTO "users"("first_name","last_name","email","is_male","birthday","height")
+    VALUES 
+      ('${user.first_name}', '${user.last_name}', '${user.email}', '${user.is_male}', '${user.birthday}', '${user.height}');
+  `);
   console.log(res);
   await client.end();
 }
