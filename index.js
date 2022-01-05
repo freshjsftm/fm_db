4f9522d0bcbe2ab94fa4346d8b939ae8c334f4d3
Elena <freshjs.ftm@gmail.com>
2022-01-05 09:25:14 +0200

    Insert many users

--- index.js
+++ index.js
@@ -8,14 +8,32 @@ const config = {
   database: 'fm_test'
 };
 
-const user = {
-  first_name: 'Tom',
+const users = [
+  {
+  first_name: 'Tim',
   last_name: 'Bom',
-  email: 'Tom@gmail.com',
+  email: 'Tim@gmail.com',
+  is_male: true,
+  birthday: '1961-06-28',
+  height: 1.85,
+},
+{
+  first_name: 'Dim',
+  last_name: 'Rom',
+  email: 'dim@gmail.com',
+  is_male: true,
+  birthday: '1961-06-28',
+  height: 1.85,
+},
+{
+  first_name: 'Bim',
+  last_name: 'Bom',
+  email: 'bim@gmail.com',
   is_male: true,
   birthday: '1961-06-28',
   height: 1.85,
 }
+]
 
 const client = new Client(config);
 start();
@@ -24,9 +42,17 @@ async function start(){
   await client.connect();
   const res = await client.query(`
     INSERT INTO "users"("first_name","last_name","email","is_male","birthday","height")
-    VALUES 
-      ('${user.first_name}', '${user.last_name}', '${user.email}', '${user.is_male}', '${user.birthday}', '${user.height}');
+    VALUES ${mapUsers(users)};
   `);
   console.log(res);
   await client.end();
 }
+
+function mapUsers(users){
+  return users
+    .map(
+      user => `('${user.first_name}', '${user.last_name}', '${user.email}', '${user.is_male}', '${user.birthday}', '${user.height}')`
+    )
+    .join(',');
+}
+//console.log(mapUsers(users));
