9fd5f8fae96fe0b55177e5d817b4f1a2dce83533
Elena <freshjs.ftm@gmail.com>
2022-01-05 10:13:26 +0200

    Add axios

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const { Client } = require('pg');
-
+const axios = require('axios');
+const http = axios.create({
+  baseURL: 'https://randomuser.me/api/'
+});
+const loadUser = async () => {
+  const {data:{results}} = await http.get('?results=1000&seed=db_fm&page=2');
+  return results;
+}
 const config = {
   user:'postgres',
   password:'postgres',
@@ -7,39 +14,11 @@ const config = {
   port: 5432,
   database: 'fm_test'
 };
-
-const users = [
-  {
-  first_name: 'Tim',
-  last_name: 'Bom',
-  email: 'Tim@gmail.com',
-  is_male: true,
-  birthday: '1961-06-28',
-  height: 1.85,
-},
-{
-  first_name: 'Dim',
-  last_name: 'Rom',
-  email: 'dim@gmail.com',
-  is_male: true,
-  birthday: '1961-06-28',
-  height: 1.85,
-},
-{
-  first_name: 'Bim',
-  last_name: 'Bom',
-  email: 'bim@gmail.com',
-  is_male: true,
-  birthday: '1961-06-28',
-  height: 1.85,
-}
-]
-
 const client = new Client(config);
 start();
-
 async function start(){
   await client.connect();
+  const users = await loadUser();
   const res = await client.query(`
     INSERT INTO "users"("first_name","last_name","email","is_male","birthday","height")
     VALUES ${mapUsers(users)};
@@ -47,12 +26,11 @@ async function start(){
   console.log(res);
   await client.end();
 }
-
 function mapUsers(users){
   return users
     .map(
-      user => `('${user.first_name}', '${user.last_name}', '${user.email}', '${user.is_male}', '${user.birthday}', '${user.height}')`
+      ({name:{first, last}, email, gender, dob:{date} }) => 
+      `('${first}', '${last}', '${email}', '${gender==='male'}', '${date}', '${(Math.random()+1.1).toFixed(2)}')`
     )
     .join(',');
 }
-//console.log(mapUsers(users));
